cdd86c7aee12904b938f63df4aad689fa645c645
Jules-2MJ <146131402+2MJ-DEV@users.noreply.github.com>
2025-02-25 16:37:35 +0100

    mis a jour app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Login from "./components/Login.jsx";
+import MapComponent from "./components/MapComponent.jsx";
+import Sidebar from "./components/Sidebar.jsx";
 
 const App = () => {
   return (
     <div>
       <Login />
+      <MapComponent />
+      <Sidebar />
     </div>
   );
 };
