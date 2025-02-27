770cdc3aab3455ca79a250039925f6e5ec678a3c
Jules-2MJ <146131402+2MJ-DEV@users.noreply.github.com>
2025-02-27 12:17:14 +0100

    Modificationd de pack-lock-json, pack-json et App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/Login.jsx";
 import MapComponent from "./components/MapComponent.jsx";
 import Sidebar from "./components/Sidebar.jsx";
 
 const App = () => {
   return (
-    <div>
-      <Login />
-      <MapComponent />
-      <Sidebar />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/map" component={MapComponent} />
+        <Route path="/sidebar" component={Sidebar} />
+      </Switch>sw
+    </Router>
   );
 };
 
