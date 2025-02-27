674edebaa0cf0d45b27bbc32aa5a8b0a2d3a3784
Jules-2MJ <146131402+2MJ-DEV@users.noreply.github.com>
2025-02-27 12:40:22 +0100

    Reinitialisation du projet

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Login from "./components/Login.jsx";
-import MapComponent from "./components/MapComponent.jsx";
-import Sidebar from "./components/Sidebar.jsx";
+import Login from "./components/Login";
+import MapComponent from "./components/MapComponent";
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/map" component={MapComponent} />
-        <Route path="/sidebar" component={Sidebar} />
-      </Switch>sw
-    </Router>
+    <div>
+      <Login />
+      <MapComponent />
+    </div>
   );
-};
+}
 
 export default App;
